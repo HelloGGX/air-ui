b1725d4950e4d7bcd9f1926b53bfce6dc15720c3
GGX <1056937524@qq.com>
2024-08-13 22:54:07 +0800

    优化配置

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,29 @@ module.exports = {
     content: ['./packages/**/*.{vue,js,ts,jsx,tsx}'],
     darkMode: ['class', '[data-mode="dark"]'],
     theme: {
-        extend: {}
+        extend: {
+            colors: {
+                // 定义节日主题颜色
+                springFestival: {
+                    primary: '#FF4B4B', // 春节主题的红色
+                    secondary: '#FFD700' // 春节主题的金色
+                },
+                christmas: {
+                    primary: '#007F0E', // 圣诞节的绿色
+                    secondary: '#FF0000' // 圣诞节的红色
+                },
+                halloween: {
+                    primary: '#FF7518', // 万圣节的橙色
+                    secondary: '#1E1E1E' // 万圣节的黑色
+                }
+            },
+            backgroundImage: {
+                // 定义节日主题背景图片
+                'spring-festival': "url('/images/spring-festival-bg.jpg')",
+                'christmas': "url('/images/christmas-bg.jpg')",
+                'halloween': "url('/images/halloween-bg.jpg')",
+              },
+        }
     },
     plugins: []
 };
