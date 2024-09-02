a512eb989f1008e36e0a37be3226baabce1ea8a7
郭高翔 <guogaoxiang@travelsky.com.cn>
2024-09-02 16:37:59 +0800

    init

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,12 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import { resolve } from 'path';
 import ElementPlus from 'unplugin-element-plus/vite';
 
 export default defineConfig({
     plugins: [vue(), ElementPlus()],
-    resolve: {
-        alias: {
-            '@': resolve(__dirname, './'),
-            '@air-element': resolve(__dirname, './packages/air-element'),
-            '@block': resolve(__dirname, './packages/block'),
-            '@page': resolve(__dirname, './packages/page'),
-            '@scaffold': resolve(__dirname, './packages/scaffold')
-        }
-    },
     css: {
         postcss: {
-            plugins: [
-                require('postcss-import'),
-                require('tailwindcss'),
-                require('autoprefixer')
-            ]
+            plugins: [require('postcss-import'), require('tailwindcss'), require('autoprefixer')]
         }
     }
 });
