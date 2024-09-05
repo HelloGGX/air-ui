7bac2ef0d74aec7ff4d03468db3a47e80cbab936
郭高翔 <guogaoxiang@travelsky.com.cn>
2024-09-05 16:31:28 +0800

    air-cli

--- packages/air-element/components/button/Button.stories.ts
+++ packages/air-element/components/button/Button.stories.ts
@@ -7,7 +7,6 @@ const meta: Meta<typeof AirButton> = {
     title: 'Component/Button',
     component: AirButton,
     tags: ['autodocs'],
-
     argTypes: {
         size: {
             control: { type: 'select' },
