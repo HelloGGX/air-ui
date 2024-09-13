a7c31f42a6f5b525fa6d48b192941966d1637188
郭高翔 <guogaoxiang@travelsky.com.cn>
2024-09-13 12:36:17 +0800

    修改镜像源

--- packages/block/tailwind.config.ts
+++ packages/block/tailwind.config.ts
@@ -1,11 +1,6 @@
-import theme from '@air-ui/air-element/tailwind.config';
+import elementTailwindConfig from '@air-ui/air-element/tailwind.config';
 
 module.exports = {
-    theme: {
-        extend: {
-            ...theme.extend
-        }
-    },
-    variants: {},
-    plugins: []
+    ...elementTailwindConfig,
+    content: ["./src/*.{vue,js,ts,jsx,tsx}",]
 };
