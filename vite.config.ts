c043b320dc9adb40a90fcd7f13b8b15e7717f50e
郭高翔 <guogaoxiang@travelsky.com.cn>
2024-09-14 11:47:16 +0800

    添加配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import path from 'path';
 
 export default defineConfig({
     plugins: [vue()],
+    resolve: {
+        alias: {
+            '@air-ui/air-element': path.resolve(__dirname, 'packages/air-element/dist'),
+            '@air-ui/block': path.resolve(__dirname, 'packages/block/dist')
+        }
+    },
     css: {
         postcss: {
             plugins: [require('postcss-import'), require('tailwindcss'), require('autoprefixer')]
