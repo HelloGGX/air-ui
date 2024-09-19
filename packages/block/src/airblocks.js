22a7e714e6939de0d218664a68cdaa0255542308
wuxuexue@travelsky.com.cn <wuxuexue@travelsky.com.cn>
2024-09-19 16:12:44 +0800

    创建airStep

--- packages/block/src/airblocks.js
+++ packages/block/src/airblocks.js
@@ -6,3 +6,7 @@ export { default as AirCard } from './aircard/AirCard.vue';
 //airseat ------------------------------
 export * from './airseat/AirSeat.vue';
 export { default as AirSeat } from './airseat/AirSeat.vue';
+
+//airstep ------------------------------
+export * from './airstep/Airstep.vue';
+export { default as Airstep } from './airstep/Airstep.vue';
---
+++ packages/block/src/airstep/Airstep.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="airstep">
+    <!-- Airstep 组件内容 -->
+    Airstep组件
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import '../theme/index.scss';
+
+// 添加逻辑
+</script>
