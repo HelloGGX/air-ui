1daf53e1b91f8e2c92ed608332143e0deef2c08e
郭高翔 <guogaoxiang@travelsky.com.cn>
2024-09-19 16:20:26 +0800

    新增添加物料的命令

--- packages/block/src/airblocks.js
+++ packages/block/src/airblocks.js
@@ -1,3 +1,7 @@
 //airseat ------------------------------
 export * from './airseat/AirSeat.vue';
 export { default as AirSeat } from './airseat/AirSeat.vue';
+
+//airseat ------------------------------
+export * from './airseat/AirSeat.vue';
+export { default as AirSeat } from './airseat/AirSeat.vue';
