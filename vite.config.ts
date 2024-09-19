fa62024b96bdb97765c612d7b70f12e6e1efc279
郭高翔 <guogaoxiang@travelsky.com.cn>
2024-09-19 11:06:41 +0800

    修改rollup配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,14 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import path from 'path';
 
 export default defineConfig({
     plugins: [vue()],
-    resolve: {
-        alias: {
-            '@air-ui/air-element': path.resolve(__dirname, 'packages/air-element/dist'),
-            '@air-ui/block': path.resolve(__dirname, 'packages/block/dist')
-        }
-    },
     css: {
+        preprocessorOptions: {
+            scss: {
+                includePaths: ['node_modules']
+            }
+        },
         postcss: {
             plugins: [require('postcss-import'), require('tailwindcss'), require('autoprefixer')]
         }
