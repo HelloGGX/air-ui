7ade70866789eefa7a3661c2dd6394f8f9ab625f
郭高翔 <1056937524@qq.com>
2025-01-21 14:24:55 +0800

    更新story

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,24 +1,29 @@
+import { dirname, join } from "path";
 /** @type { import('@storybook/vue3-vite').StorybookConfig } */
 
 const config = {
     stories: ['../packages/**/*.stories.@(js|jsx|ts|tsx)'],
     addons: [
-        '@storybook/addon-docs',
-        '@storybook/addon-onboarding',
-        '@storybook/addon-links',
-        '@storybook/addon-essentials',
-        '@chromatic-com/storybook',
-        '@storybook/addon-interactions',
-        '@storybook/addon-themes'
+        getAbsolutePath("@storybook/addon-docs"),
+        getAbsolutePath("@storybook/addon-onboarding"),
+        getAbsolutePath("@storybook/addon-links"),
+        getAbsolutePath("@storybook/addon-essentials"),
+        getAbsolutePath("@chromatic-com/storybook"),
+        getAbsolutePath("@storybook/addon-interactions"),
+        getAbsolutePath("@storybook/addon-themes")
     ],
 
     framework: {
-        name: '@storybook/vue3-vite'
+        name: getAbsolutePath("@storybook/vue3-vite")
     },
     core: {
         builder: {
-            name: '@storybook/builder-vite'
+            name: getAbsolutePath("@storybook/builder-vite")
         }
     }
 };
 export default config;
+
+function getAbsolutePath(value) {
+    return dirname(require.resolve(join(value, "package.json")));
+}
