0ee05c9bf5342ff4b2453feffc5ad7c90a467899
郭高翔 <1056937524@qq.com>
2025-02-26 10:51:11 +0800

    init

--- packages/block/src/airseat/AirSeat.d.ts
+++ packages/block/src/airseat/AirSeat.d.ts
@@ -10,8 +10,8 @@ import { VNode } from 'vue';
 
 export interface AirSeatProps {
     status: 'available' | 'selected' | 'unavailable' | 'emergency-left' | 'emergency-right';
-    seatNumber: number;
-    seatOwnerIndex: number;
+    seatNumber?: number;
+    seatOwnerIndex?: number;
 }
 export interface ISeat {
     seatRow: string | null;
